5630de288a404fe4563dc7270b27074b863e7af2
Ahnaf-dev <ahnafbari99@gmail.com>
2024-04-17 10:51:12 -0600

    update

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  next()
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
+    next()
 })
 
 app.use("/api/users", require("./routes/auth"));
